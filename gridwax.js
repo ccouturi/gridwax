8f3c62c2eac1a047b2f8ec795de74bc46c0f3683
cyril <cyril78@gmail.com>
2013-10-19 20:36:45 +0200

    Update gridwax.js
    Default line-height to 21px.

--- gridwax.js
+++ gridwax.js
@@ -46,7 +46,7 @@
       });
       this.gridline = "<div class='gw-gridline'></div>";
       $("body").css("padding-bottom", "35px");
-      $("body").append("<div id='gw-overlay'>                        <img src='http://dl.dropbox.com/u/37716909/moustache.png'/>                        <div id='gw-console' class='gw-razor'>                          <label>Line-height: <input type='text' class='gw-lh' value='18' /></label>                          <label>Offset: <input type='text' class='gw-o' value='0' /></label>                          <label>Color:                            <a class='color'>#efefef</a>                            <a class='color'>#000</a>                            <a class='color'>#bd424b</a>                            <a class='color'>#cb6020</a>                            <a class='color'>#ffe960</a>                            <a class='color'>#3f9679</a>                            <a class='color'>#4c8dcf</a>                            <a class='color'>#a672ae</a>                            <a class='color'>#7576c9</a>                          </label>                        </div>                        <div id='gw-close' class='gw-razor'><a href='#' onclick='window.gridwax.shave();'>Remove the Grid ✖</a></div>                      </div>");
+      $("body").append("<div id='gw-overlay'>                        <img src='http://dl.dropbox.com/u/37716909/moustache.png'/>                        <div id='gw-console' class='gw-razor'>                          <label>Line-height: <input type='text' class='gw-lh' value='21' /></label>                          <label>Offset: <input type='text' class='gw-o' value='0' /></label>                          <label>Color:                            <a class='color'>#efefef</a>                            <a class='color'>#000</a>                            <a class='color'>#bd424b</a>                            <a class='color'>#cb6020</a>                            <a class='color'>#ffe960</a>                            <a class='color'>#3f9679</a>                            <a class='color'>#4c8dcf</a>                            <a class='color'>#a672ae</a>                            <a class='color'>#7576c9</a>                          </label>                        </div>                        <div id='gw-close' class='gw-razor'><a href='#' onclick='window.gridwax.shave();'>Remove the Grid ✖</a></div>                      </div>");
       this.overlay = $("#gw-overlay");
       this.overlay.css({
         position: "fixed",
@@ -140,7 +140,7 @@
         "margin-right": "10px",
         "margin-top": "4px"
       });
-      return this.setGrid(18, 0);
+      return this.setGrid(21, 0);
     };
 
     GridWax.prototype.shave = function(e) {
